16a7c6b2683329e6f92875823e9a9c050e566991
darekaze <darekaze@gmail.com>
2019-04-25 11:20:27 +0800

    Add start template

---
+++ wxcs/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired, InputRequired, Length
+
+class RegistrationForm(FlaskForm):
+  name = StringField(u'Name',
+    validators=[DataRequired(), Length(min=2, max=30)])
+  post = StringField(u'Post',
+    validators=[DataRequired(), Length(min=1, max=20)])
+  cases = SelectField(u'Weather Case', coerce=int)
+  role = SelectField(u'Role', coerce=int,
+    validators=[InputRequired()],
+    choices=[(0, 'Forecaster'), (1, 'TC Consultant'), (2, 'Assistant Forecaster')]
+  )
+  submit = SubmitField('Start')
--- wxcs/routes.py
+++ wxcs/routes.py
@@ -1,7 +1,25 @@
 from flask import render_template, url_for, flash, redirect
 from wxcs import app
+from wxcs.forms import RegistrationForm
 
-@app.route("/")
-@app.route("/home")
-def hello_world():
-    return render_template('home.jinja')
+app.config['SECRET_KEY'] = 'lalalalala' # HACK: temp
+
+"""
+Render register page on start, and prevent register and redirect
+user to progress while simulation begins
+"""
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/start", methods=['GET', 'POST'])
+def register():
+  form = RegistrationForm()
+  form.cases.choices = [(1, 'Choose...')]
+  # TODO: fetch weather case selections from database
+
+  if form.validate_on_submit():
+    flash(f'Drill Start!! {form.name.data}', 'success')
+    return redirect(url_for('temp'))
+  return render_template('start.jinja', form=form)
+
+@app.route("/temp")
+def temp():
+  return render_template('temp.jinja')
